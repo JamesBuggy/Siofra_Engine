06355fc5f673fe781630429a0da2d76464ca6b47
James Buggy <jamesb09dev@gmail.com>
2022-06-30 20:09:50 +0100

    Added initial custom vector3 AB#118

---
+++ engine/include/math/vector3.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+namespace siofraEngine::math
+{
+	class Vector3
+	{
+	public:
+		Vector3(float x, float y, float z) : x{ x }, y{ y }, z{ z } { };
+
+		Vector3 operator + (const Vector3& other) const noexcept { return Vector3{ x + other.x, y + other.y, z + other.z }; }
+		Vector3 operator - (const Vector3& other) const noexcept { return Vector3{ x - other.x, y - other.y, z - other.z }; }
+		Vector3 operator * (float scalar) const noexcept { return Vector3{ x * scalar, y * scalar, z * scalar }; }
+		Vector3 operator / (float scalar) const { return Vector3{ x / scalar, y / scalar, z / scalar }; }
+
+		void operator += (const Vector3& other) noexcept { x += other.x; y += other.y; z += other.z; }
+		void operator -= (const Vector3& other) noexcept { x -= other.x; y -= other.y; z -= other.z; }
+		void operator *= (float scalar) noexcept { x *= scalar; y *= scalar; z *= scalar; }
+		void operator /= (float scalar) { x /= scalar; y /= scalar; z /= scalar; }
+
+		float x{ 0 };
+		float y{ 0 };
+		float z{ 0 };
+	};
+}
