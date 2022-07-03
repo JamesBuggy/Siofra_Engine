3612d4ed840fa6b12f0dfd1f19eddd906199a599
James Buggy <jamesb09dev@gmail.com>
2022-07-03 11:50:13 +0100

    Added initial vector 4 AB#121

---
+++ engine/include/math/vector4.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "math/vector3.hpp"
+
+namespace siofraEngine::math
+{
+	class Vector4
+	{
+	public:
+		Vector4(float x, float y, float z, float w) : x{ x }, y{ y }, z{ z }, w{ w } { };
+		Vector4(Vector3 vector3, float w) : x{ vector3.x }, y{ vector3.y }, z{ vector3.z }, w{ w } { };
+
+		Vector4 operator + (const Vector4& other) const noexcept { return Vector4{ x + other.x, y + other.y, z + other.z, w + other.w }; }
+		Vector4 operator - (const Vector4& other) const noexcept { return Vector4{ x - other.x, y - other.y, z - other.z, w - other.w }; }
+		Vector4 operator * (float scalar) const noexcept { return Vector4{ x * scalar, y * scalar, z * scalar, w * scalar }; }
+		Vector4 operator / (float scalar) const { return Vector4{ x / scalar, y / scalar, z / scalar, w / scalar }; }
+
+		void operator += (const Vector4& other) noexcept { x += other.x; y += other.y; z += other.z; w += other.w; }
+		void operator -= (const Vector4& other) noexcept { x -= other.x; y -= other.y; z -= other.z; w -= other.w; }
+		void operator *= (float scalar) noexcept { x *= scalar; y *= scalar; z *= scalar; w *= scalar; }
+		void operator /= (float scalar) { x /= scalar; y /= scalar; z /= scalar; w /= scalar; }
+
+		float x{ 0 };
+		float y{ 0 };
+		float z{ 0 };
+		float w{ 0 };
+	};
+}
