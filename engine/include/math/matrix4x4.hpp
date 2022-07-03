031651772539b2aef800e7d4e2a64b5a0bff2c9b
James Buggy <jamesb09dev@gmail.com>
2022-07-03 16:23:34 +0100

    Added perspective matrix generation AB#116

--- engine/include/math/matrix4x4.hpp
+++ engine/include/math/matrix4x4.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <math.h>
 #include <algorithm>
 #include <array>
 
@@ -8,6 +9,13 @@ namespace siofraEngine::math
 	class Matrix4x4
 	{
 	public:
+		Matrix4x4() = default;
+		Matrix4x4(std::array<float, 16> elements) :
+			elements{ std::move(elements) }
+		{
+		
+		}
+
 		Matrix4x4 operator + (const Matrix4x4& other) const noexcept
 		{
 			Matrix4x4 newMatrix{ };
@@ -70,6 +78,39 @@ namespace siofraEngine::math
 			{
 				elements[i] *= scalar;
 			}
+		}
+
+		/**
+		 * @brief Create a perspective projection matrix
+		 *
+		 * @param fov The field of view, in the y direction, expressed in radians
+		 * @param aspect The aspect ration determining the field of view in the x direction (width / height)
+		 * @param near The distance, from the viewer, to the near clipping plane
+		 * @param far The distance, from the viewer, to the far clipping plane
+		 * @returns The perspective matrix, or an identity matrix in the case of invalid input
+		 */
+		static Matrix4x4 perspective(float fov, float aspect, float near, float far) noexcept
+		{
+			float frustumDepth = far - near;
+
+			if (fov <= 0.0f || aspect == 0.0f || frustumDepth <= 0.0f || near <= 0.0f || far <= 0.0f)
+			{
+				return Matrix4x4
+				{{
+					1, 0, 0, 0,
+					0, 1, 0, 0,
+					0, 0, 1, 0,
+					0, 0, 0, 1
+				}};
+			}
+
+			Matrix4x4 perpectiveMatrix{ };
+			perpectiveMatrix.elements[5] = 1.0f / tanf(fov / 2);
+			perpectiveMatrix.elements[0] = 1.0f * perpectiveMatrix.elements[5] / aspect;
+			perpectiveMatrix.elements[10] = -((far + near) / frustumDepth);
+			perpectiveMatrix.elements[11] = -1.0f;
+			perpectiveMatrix.elements[14] = -((2.0f * far * near) / frustumDepth);
+			return perpectiveMatrix;
 		}
 
 		std::array<float, 16> elements{ };
